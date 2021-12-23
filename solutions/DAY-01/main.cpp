a58186f423601ef4e96041d7d29045f539b3db02
an-dyy <andy.development@protonmail.com>
2021-12-22 19:24:56 -0500

    perf: Use `intLines` instead of `getLines`

--- solutions/DAY-01/main.cpp
+++ solutions/DAY-01/main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 aoc::Helper helper;
 
 
-int solveInput(vector<string> lines, int start, int back) {
+int solveInput(vector<int> lines, int start, int back) {
     int increased = 0;
     
     for (int idx = start; idx < lines.size(); ++idx) {
-        if (stoi(lines[idx]) > stoi(lines[idx - back])) {
+        if (lines[idx] > lines[idx - back]) {
             ++increased;
         }
     }
@@ -18,7 +18,7 @@ int solveInput(vector<string> lines, int start, int back) {
 
 
 int main() {
-    vector<string> lines = helper.getLines("./input.txt");
+    vector<int> lines = helper.intLines("./input.txt");
 
     printf("Part 1: %i\n", solveInput(lines, 1, 1));
     printf("Part 2: %i\n", solveInput(lines, 3, 3));
